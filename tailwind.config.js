b4c8efe9ae5834a4a1d9243b736286bde4475f12
melnikov <mel161@yandex.ru>
2023-10-20 12:20:11 +0500

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+  ],
+  theme: {
+    container: {
+      center: true
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
